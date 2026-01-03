b8d4229f5ced212215a516fd3744f188037de545
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2026-01-03 16:35:44 +0530

    ROuting implementation for side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
+import { AboutComponent } from './screens/about/about.component';
+import { ProjectComponent } from './screens/project/project.component';
+import { ContactComponent } from './screens/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'project', component: ProjectComponent },
+    { path: 'contact', component: ContactComponent },
+
     { path: '**', redirectTo: '' }
 
 ];
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterLink, RouterLinkActive, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
